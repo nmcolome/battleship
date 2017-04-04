ed32b1e78b6419d9b13b1e369258516af5b85dc6
NataliaColome <nmcolome@gmail.com>
2017-04-03 23:24:13 -0600

    added ship_placement_prompt

--- lib/messages.rb
+++ lib/messages.rb
@@ -21,5 +21,14 @@ module Messages
     puts "inter(m)ediate = 8x8 grid, 2-unit boat, 3-unit boat, 4-unit boat"
     puts "(a)dvanced = 12x12 grid, 2-unit boat, 3-unit boat, 4-unit boat, 5-unit boat"
   end
+
+  def ship_placement_prompt
+    puts "I have laid out my ships on the grid."
+    puts "You now need to layout your two ships."
+    puts "The first is two units long and the second is three units long."
+    puts "The grid has A1 at the top left and D4 at the bottom right."
+    puts
+    puts "Enter the squares for the two-unit ship:"
+  end
   
 end
