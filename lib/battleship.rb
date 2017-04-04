6a54548f8fdbe135e8f736f5221178b639710a8a
NataliaColome <nmcolome@gmail.com>
2017-04-03 23:22:59 -0600

    refactored code into 3 methods

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,22 +1,59 @@
 require './lib/messages.rb'
+require './lib/ships.rb'
+require './lib/grid.rb'
+require './lib/cell.rb'
 
-# class Battleship
+class Battleship
   include Messages
+  include ShipPlacement
 
-  puts welcome
-  action = gets.chomp
+  def initialize
+    
+  end
 
-  if action == "p" || action == "play"
-    level
-    action = gets.chomp
-  elsif action == "i" || action == "instructions"
-    instructions
-    puts "press ENTER to return"
-    action = gets
+  def welcome_message
     puts welcome
     action = gets.chomp
-  else action == "q" || action == "quit"
-    quit
+      case action
+    when "p" || "play"
+      level_selection
+    when "i" #|| "instructions"
+      instructions
+      puts
+      puts
+      welcome_message
+    when "q" || "quit"
+      quit
+    end
   end
-  
-# end
+
+  def level_selection
+    level
+    selection = gets.chomp
+      case selection
+    when "a" || "advanced"
+      game_setup(12,5)
+    when "m" || "intermediate"
+      game_setup(8,4)
+    when "b" || "beginner"
+      game_setup(4,2)
+    end
+  end
+
+  def game_setup(board_size, num_of_ships)
+    ships_types = ["patrol", "submarine", "battleship", "destroyer", "carrier"]    
+    board = Grid.new(board_size)
+    computer_ships = ships_types[0..num_of_ships - 1].map {|type| Ships.new(type)}
+    users_ships = ships_types[0..num_of_ships - 1].map {|type| Ships.new(type)}
+    computer_ship_placement(computer_ships)
+  end
+
+  def computer_ship_placement(computer_ships)
+    computer_ships.each do |ship|
+      random_comp_coordinates(ship.type, ship.magnitude)
+      ship_placement_coordinates(ship.head, ship.tail)
+      ship_setup(y_start, x_start, y_end, x_end)
+    end
+    board.print_grid    
+  end
+end
