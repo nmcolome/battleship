7a843716cce3a46ca9b18895ef07416cedd1669f
NataliaColome <nmcolome@gmail.com>
2017-04-04 19:01:42 -0600

    change the way to create a grid and set default value to " "

--- lib/grid.rb
+++ lib/grid.rb
@@ -6,9 +6,9 @@ class Grid
   attr_reader :grid
   attr_accessor :board_size
 
-  def initialize(board_size, value = ".")
+  def initialize(board_size, value = " ")
     @board_size = board_size
-    @grid = Array.new(board_size, Array.new(board_size, value))
+    @grid = Array.new(board_size) { Array.new(board_size, value)}
   end
 
   def total_rows
@@ -37,5 +37,9 @@ class Grid
     symbol = "==="
     borders = Array.new(board_size + 1, symbol)
   end
+
+  # def update_grid(grid)
+  #   grid.print_grid
+  # end
 
 end
