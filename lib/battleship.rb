3de013bbcf42dbe70ecf3f0f42e71d0de69e4d50
NataliaColome <nmcolome@gmail.com>
2017-04-04 19:00:33 -0600

    removed methods from ship_placement and added them to battleship

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -1,15 +1,14 @@
 require './lib/messages.rb'
+require './lib/ship_placement.rb'
+
 require './lib/ships.rb'
 require './lib/grid.rb'
 require './lib/cell.rb'
+require 'pry'
 
 class Battleship
   include Messages
   include ShipPlacement
-
-  def initialize
-    
-  end
 
   def welcome_message
     puts welcome
@@ -17,7 +16,7 @@ class Battleship
       case action
     when "p" || "play"
       level_selection
-    when "i" #|| "instructions"
+    when "i" || "instructions"
       instructions
       puts
       puts
@@ -33,27 +32,96 @@ class Battleship
       case selection
     when "a" || "advanced"
       game_setup(12,5)
+      message_setup(12,5)
     when "m" || "intermediate"
       game_setup(8,4)
+      message_setup(8,4)
     when "b" || "beginner"
       game_setup(4,2)
+      message_setup(4,2)
     end
   end
 
   def game_setup(board_size, num_of_ships)
-    ships_types = ["patrol", "submarine", "battleship", "destroyer", "carrier"]    
-    board = Grid.new(board_size)
+    ships_types = ["patrol", "submarine", "destroyer", "battleship", "carrier"]    
+    comp_board = Grid.new(board_size)
+    user_board = Grid.new(board_size)
     computer_ships = ships_types[0..num_of_ships - 1].map {|type| Ships.new(type)}
-    users_ships = ships_types[0..num_of_ships - 1].map {|type| Ships.new(type)}
-    computer_ship_placement(computer_ships)
+    user_ships = ships_types[0..num_of_ships - 1].map {|type| Ships.new(type)}
+    computer_validation(computer_ships, comp_board)
   end
 
-  def computer_ship_placement(computer_ships)
-    computer_ships.each do |ship|
-      random_comp_coordinates(ship.type, ship.magnitude)
-      ship_placement_coordinates(ship.head, ship.tail)
-      ship_setup(y_start, x_start, y_end, x_end)
-    end
-    board.print_grid    
+  def computer_layout
   end
+  
+
+
+
+  def computer_validation(computer_ships, comp_board) #pendiente de validar q no haya overlap
+    comp_board_size = comp_board.board_size
+    #inicio de loop q no se como hacer
+    comp_ship_coord = computer_ships.map do |ship|
+      random_comp_coordinates(ship, ship.magnitude, comp_board_size)
+    end
+    # computer_ships.map do |ship|
+    #   coordinates = []
+    #   coordinates << ship.coordinates(0) 
+    #   coordinates << ship.coordinates(1)
+    #   end
+    # if coordinates.uniq! !=nil
+    #   comp_ship_coord = computer_ships.map do |ship|
+    #   random_comp_coordinates(ship, ship.magnitude, comp_board_size)
+    # end
+    # end
+    computer_ships.map do |ship|
+      ship_setup(ship, comp_board)
+    end
+    p comp_board.print_grid #eliminar
+  end
+
+  def random_comp_coordinates(ship, magnitude, board_size)
+    position = rand(2)
+    if position == 0 #horizontal
+      row = rand(board_size)
+      column = rand(board_size - magnitude + 1)
+      ship << Cell.new("#{row}#{column}")
+      ship << Cell.new("#{row}#{column + magnitude - 1}")
+    else #vertical
+      row = rand(board_size - magnitude + 1)
+      column = rand(board_size)
+      ship << Cell.new("#{row}#{column}")
+      ship << Cell.new("#{row + magnitude - 1}#{column}")
+    end
+  end
+
+  # def ship_placement_coordinates(head, tail)
+  #   starting = head.split(//)
+  #   ending = tail.split(//)
+  #   y_start = columns_key.index(starting[0])
+  #   x_start = rows_key.index(starting[1])
+  #   y_end = columns_key.index(ending[0])
+  #   x_end = rows_key.index(ending[1])
+  #   coordinates = [y_start, x_start, y_end, x_end]
+  # end
+
+  def ship_setup(ship, player_board)
+    if ship.row(0) == ship.row(1)
+      player_board.grid[ship.row(0).to_i][ship.column(0).to_i] = "<"
+      player_board.grid[ship.row(1).to_i][ship.column(1).to_i] = ">"
+      interior = ship.column(0).next.to_i
+      until interior == ship.column(1).to_i
+        player_board.grid[ship.row(0).to_i][interior] = "*"
+        interior += 1
+      end
+    else
+      player_board.grid[ship.row(0).to_i][ship.column(0).to_i] = "^"
+      player_board.grid[ship.row(1).to_i][ship.column(1).to_i] = "v"
+      interior_row = ship.row(0).next.to_i
+      until interior_row == ship.row(1).to_i
+        player_board.grid[interior_row][ship.column(0).to_i] = "*"
+        interior_row += 1
+      end
+    end
+  end
+
 end
