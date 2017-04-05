6c9a0ef21d01e4cf58111ef48db0492c7e50f5e7
NataliaColome <nmcolome@gmail.com>
2017-04-04 19:02:34 -0600

    Added a message to prompt the user to enter coordinates

--- lib/messages.rb
+++ lib/messages.rb
@@ -19,16 +19,31 @@ module Messages
     puts "What difficulty level would you like to play?"
     puts "(b)eginner = 4x4 grid, 2-unit boat, 3-unit boat"
     puts "inter(m)ediate = 8x8 grid, 2-unit boat, 3-unit boat, 4-unit boat"
-    puts "(a)dvanced = 12x12 grid, 2-unit boat, 3-unit boat, 4-unit boat, 5-unit boat"
+    puts "(a)dvanced = 12x12 grid, 2-unit boat, 3-unit boat, 3-unit boat, 4-unit boat, 5-unit boat"
   end
 
-  def ship_placement_prompt
+  def message_setup(board_size, ships)
     puts "I have laid out my ships on the grid."
-    puts "You now need to layout your two ships."
-    puts "The first is two units long and the second is three units long."
-    puts "The grid has A1 at the top left and D4 at the bottom right."
+    puts "You now need to layout your #{ships} ships."
+    if ships == 5
+      puts "The first is the patrol (2-units long), then the submarine (3-units), then the destroyer (3-units), then the battleship (4-units) and finally the carrier (5-units long)"
+      puts "The grid is #{board_size}x#{board_size}, it has A1 at the top left and L12 at the bottom right."
+    elsif 
+      ships == 4
+      puts "The first is the patrol (2-units long), then the submarine (3-units), then the destroyer (3-units), and finally the battleship (4-units long)"
+      puts "The grid is #{board_size}x#{board_size}, it has A1 at the top left and H8 at the bottom right."
+    elsif 
+      ships == 2
+      puts "The first is the patrol (2-units long) and the second is the submarine (3-units long)"
+      puts "The grid is #{board_size}x#{board_size}, it has A1 at the top left and D4 at the bottom right."
+    end
     puts
-    puts "Enter the squares for the two-unit ship:"
+    
+    ships_details = ["patrol (2-units)", "submarine (3-units)", "destroyer (3-units)", "battleship (4-units)", "carrier (5-units)"]
+    ships_details[0..ships-1].each do 
+      puts "Enter the starting and ending squares for the #{ship} ship:"
+      user_coordinates = gets.chomp
+    end
   end
   
 end
