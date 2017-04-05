dc78e752fb692aee56aadaa2d04dc31aafb94d68
NataliaColome <nmcolome@gmail.com>
2017-04-04 19:05:36 -0600

    ongoing definition of edit_specific_cell

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -23,22 +23,30 @@ class GridTest < Minitest::Test
     assert_equal 4, board.total_columns
   end
   
-  def test_if_print_the_board_correctly #how to do this
-    skip
-    board = Grid.new(8)
-
-    desired_output = W
-===========================
-*  A  B  C  D  E  F  G  H
-1  ^  ^  ^  ^  ^  ^  ^  ^
-2  ^  ^  ^  ^  ^  ^  ^  ^
-3  ^  ^  ^  ^  ^  ^  ^  ^
-4  ^  ^  ^  ^  ^  ^  ^  ^
-5  ^  ^  ^  ^  ^  ^  ^  ^
-6  ^  ^  ^  ^  ^  ^  ^  ^
-7  ^  ^  ^  ^  ^  ^  ^  ^
-8  ^  ^  ^  ^  ^  ^  ^  ^
-===========================
-    assert_equal desired_output, board.print_grid
+  def test_if_edit_a_specific_cell
+    board = Grid.new(10)
+    rowss = board.grid[1]
+    rowss[1] = "x"
+    p board.grid
+    assert_equal "x", board.grid[1][1]
   end
+
+#   def test_if_print_the_board_correctly #how to do this
+#     skip
+#     board = Grid.new(8)
+
+#     desired_output = W
+# ===========================
+# *  A  B  C  D  E  F  G  H
+# 1  ^  ^  ^  ^  ^  ^  ^  ^
+# 2  ^  ^  ^  ^  ^  ^  ^  ^
+# 3  ^  ^  ^  ^  ^  ^  ^  ^
+# 4  ^  ^  ^  ^  ^  ^  ^  ^
+# 5  ^  ^  ^  ^  ^  ^  ^  ^
+# 6  ^  ^  ^  ^  ^  ^  ^  ^
+# 7  ^  ^  ^  ^  ^  ^  ^  ^
+# 8  ^  ^  ^  ^  ^  ^  ^  ^
+# ===========================
+#     assert_equal desired_output, board.print_grid
+#   end
 end
