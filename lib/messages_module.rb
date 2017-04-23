e210dacbdf2effe796a9fe4353e0a1f5c91ac8c2
NataliaColome <nmcolome@gmail.com>
2017-04-22 18:00:46 -0600

    add message for prompting difficulty level

--- lib/messages_module.rb
+++ lib/messages_module.rb
@@ -15,4 +15,8 @@ module Messages
   def request_action
     puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
   end
+
+  def difficulty_level
+    puts "Which difficulty level would you like to play:\n* (b)eginner = 4x4 grid, 2-unit boat, 3-unit boat\n* (i)ntermediate = 8x8 grid, 2-unit boat, 3-unit boat, 4-unit boat\n (a)dvanced = 12x12 grid, 2-unit boat, 3-unit boat, 4-unit boat, 5-unit boat"
+  end
 end
